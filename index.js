a927d38f44ee7744ab2ae592d4d1647a48c4be6a
Connor Holly <connorholly11@gmail.com>
2019-10-02 13:57:17 -0500

    created validate user and user ID, got the server and index set up, got to get validatePost working

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // code away!
+const server = require("./server");
+
+server.listen(5000, () => {
+  console.log("api running on port 5000");
+});
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,73 @@
-const express = 'express';
+const express = require("express");
+const db = require("./userDb");
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post("/", validateUser, (req, res) => {
+  const newUser = req.body;
 
+  db.insert(newUser)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "THERE WAS AN ERROR CREATING A NEW USER"
+      });
+    });
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post("/:id/posts", validateUserId, (req, res) => {});
 
+router.get("/", (req, res) => {
+  db.get()
+    .then(user => {
+      res.status(200).json(user);
+    })
+    .catch(error => {
+      res.status(500).json({
+        error: error,
+        message: "THERE WAS AN ERROR GETTING USERS"
+      });
+    });
 });
 
-router.get('/', (req, res) => {
-
+router.get("/:id", validateUserId, (req, res) => {
+  res.send("validation ID is working");
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id/posts", validateUserId, (req, res) => {});
 
-});
+router.delete("/:id", validateUserId, (req, res) => {});
 
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
-});
+router.put("/:id", validateUserId, (req, res) => {});
 
 //custom middleware
 
 function validateUserId(req, res, next) {
+  id = req.params.id;
 
-};
+  if (id) {
+    req.user = id;
+    next();
+  } else {
+    res.status(400).json({ message: "invalid user id" });
+  }
+}
 
 function validateUser(req, res, next) {
+  const user = req.body;
 
-};
+  if (user) {
+    next();
+  } else {
+    res.status(400).json({ message: "missing required name field" });
+  }
+}
 
 function validatePost(req, res, next) {
-
-};
+  next();
+}
 
 module.exports = router;
