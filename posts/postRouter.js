1669758487fb20602eb4d2571488fd7230224718
Connor Holly <connorholly11@gmail.com>
2019-10-03 16:12:50 -0500

    done with stretch

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -4,17 +4,49 @@ const db = require("./postDb");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("Im in the post");
+  db.get().then(posts => {
+    res.status(200).json(posts);
+  });
 });
 
-router.get("/:id", (req, res) => {});
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  db.getById(id).then(post => {
+    res.status(200).json(post);
+  });
+});
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
 
-router.put("/:id", (req, res) => {});
+  db.remove(id).then(deleted => {
+    res.status(204).json(deleted);
+  });
+});
+
+router.put("/:id", validatePostId, (req, res) => {
+  const id = req.params.id;
+  const changes = req.body;
+
+  db.update(id, changes).then(updated => {
+    res.status(200).json(updated);
+  });
+});
 
 // custom middleware
 
-function validatePostId(req, res, next) {}
+function validatePostId(req, res, next) {
+  const id = req.params.id;
+
+  db.getById(id).then(id => {
+    if (id) {
+      next();
+    } else {
+      res.status(404).json({
+        message: "404 error, invalid ID"
+      });
+    }
+  });
+}
 
 module.exports = router;
